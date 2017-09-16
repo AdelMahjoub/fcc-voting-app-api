698b7d891a2215ab4c33b4bbe9ca08bf194db08d
Adel Mahjoub <dragonhunter250@gmail.com>
2017-09-16 12:28:34 +0200

    use body-parser

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app
   .set('json spaces', 2)
   .enable('trust proxy')
   .use(helmet())
+  .use(bodyParser.json())
   .use(api)
   .listen(app.get('port'), () =>{
     console.log(`Server running\nPort: ${app.get('port')}\nMode: ${app.get('env')}`);
