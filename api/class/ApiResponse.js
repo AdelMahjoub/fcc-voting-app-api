145f73b03d83caea09f0a1abc3d57ca0979ae7bb
Adel Mahjoub <dragonhunter250@gmail.com>
2017-09-17 15:35:00 +0200

    Commented

--- api/class/ApiResponse.js
+++ api/class/ApiResponse.js
@@ -1,10 +1,13 @@
+/**
+ * api response format or interface (Now now, this a class not interface)
+ */
 class ApiResponse {
   /**
    * 
    * @param {{req: Request, success: boolean, errors: [], data: any, time: string}} props 
    */
   constructor(props = {}) {
-    this.requested = props['req'] ? `${props['req']['method']} '${props['req']['url']}'` : '';
+    this.requested = props['req'] ? `${props['req']['method']} ${props['req']['url']}` : '';
     this.success = props['success'] || false;
     this.errors = props['errors'] || [];
     this.data = props['data'] || null;
