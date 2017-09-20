c0271b73b3e9f087b018a2beb35361e8b588a160
Adel Mahjoub <dragonhunter250@gmail.com>
2017-09-20 15:06:26 +0200

    added update users and confirm users

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require('../../models/user.model');
 const ApiResponse = require('../class/ApiResponse');
 const mailer = require('../class/Mailer');
 const ValidatorGuard = require('../class/ValidatorGuard');
+const assert = require('assert');
 
 class UserController {
 
@@ -74,7 +75,7 @@ class UserController {
             return;
           })
       });
-      // The response for a succeful user registration is not sent after sending an the confirmation token
+      // The response for a succeful user registration is sent before sending the confirmation token
       // because sending an email could be slow
       const data = Object.assign({}, results, {info: ` a confirm token has been sent to ${candidate.email}`});
       res.json(new ApiResponse({req, success: true, data }));
@@ -111,11 +112,22 @@ class UserController {
     // request body and request id param are already sanitized
     // keep only the concerned keys in req.body
     const concernedFields = ['username', 'email', 'password'];
+    let properties = 0;
     Object.keys(req.body).forEach(key => {
       if(!concernedFields.includes(key)) {
         delete req.body[key];
       }
     });
+    Object.keys(req.body).forEach(key => {
+      properties++;
+    });
+    if(properties === 0) {
+      return res.json(new ApiResponse({
+        success: false,
+        req,
+        errors: ['Nothing to update']
+      }));
+    }
     User.update(req.params.id, req.body, (err, results) => {
       if(err) {
         return next(err);
@@ -128,6 +140,71 @@ class UserController {
     });
   }
 
+  /**
+   * Activated user account
+   * @param {Request} req 
+   * @param {Response} res 
+   * @param {callback} next 
+   */
+  static confirmUser(req, res, next) {
+    User.getByEmailOrUsername(req.body.identifier, (err, user) => {
+      if(err) {
+        return next(err);
+      }
+      if(!Boolean(user)) {
+        return res.json(new ApiResponse({
+          req,
+          success: false,
+          errors: ['Invalid identifier or password or confirm token']
+        }));
+      }
+      User.comparePasswords(req.body.password, user.password, (err, isMatch) => {
+        if(err) {
+          return next(err);
+        }
+        if(!isMatch) {
+          return res.json(new ApiResponse({
+            req,
+            success: false,
+            errors: ['Invalid identifier or password or confirm token']
+          }));
+        }
+        if(!(req.body.confirmToken === user.confirmToken)) {
+          return res.json(new ApiResponse({
+            req,
+            success: false,
+            errors: ['Invalid identifier or password or confirm token']
+          }));
+        }
+        if(Boolean(user.confirmed)) {
+          return res.json(new ApiResponse({
+            req,
+            success: false,
+            errors: ['Already confirmed']
+          }));
+        }
+        User.confirm(user.id, (err, results) => {
+          if(err) {
+            return next(err);
+          }
+          mailer.sendAccountActivation(user, req)
+            .then(info => {
+              console.log(info);
+              return;
+            })
+            .catch(err => {
+              console.log(err);
+              return;
+            })
+          return res.json(new ApiResponse({
+            req,
+            success: true,
+            data: Object.assign({}, results, {info: `A success account activation mail was sent to ${user.email}`})
+          }));
+        });
+      });
+    });
+  }
 }
 
 module.exports = UserController;
