c42c67eed533c43de2241ec695b390677ea6ff28
Adel Mahjoub <dragonhunter250@gmail.com>
2017-09-22 11:46:16 +0200

    mocked

--- api/votes.api.js
+++ api/votes.api.js
@@ -4,8 +4,8 @@ const votesApi = express.Router();
 
 votesApi
   .route('/api/votes')
-  .get((req, res, next) => {
-    res.json({desc: 'return all votes', scope: 'admins only'});
+  .post((req, res, next) => {
+    res.json({desc: 'vote in a poll', scope: 'auth users'});
   })
 
 votesApi
@@ -13,23 +13,11 @@ votesApi
   .get((req, res, next) => {
     res.json({desc: 'get all votes of a user', scope: 'concerned auth user and admins'});
   })
-  .delete((req, res, next) => {
-    res.json({desc: 'retract all votes', scope: 'concerned auth user and admins'});
-  })
 
 votesApi
   .route('/api/votes/users/:userId/polls/:pollId')
   .get((req, res, next) => {
     res.json({desc: 'return the voted option', scope: 'concerned auth user and admins'})
-  })
-  .delete((req, res, next) => {
-    res.json({desc: 'retract a vote', scope: 'concerned auth user and admins'});
-  })
-
-votesApi
-  .route('/api/votes/users/:userId/polls/:pollId/options/:optionId')
-  .post((req, res, next) => {
-    res.json({desc: 'vote in a poll', scope: 'auth users and admins'});
   })
 
 module.exports = votesApi;
