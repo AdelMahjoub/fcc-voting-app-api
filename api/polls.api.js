3678011845166604b6b4481b567ab4cec46e4dbe
Adel Mahjoub <dragonhunter250@gmail.com>
2017-09-22 11:33:53 +0200

    implemented

--- api/polls.api.js
+++ api/polls.api.js
@@ -2,25 +2,13 @@ const express = require('express');
 
 const pollsApi = express.Router();
 
-const Poll = require('../models/poll.model');
+const PollController = require('./controllers/PollController');
 const ValidatorGuard = require('./class/ValidatorGuard');
 const AuthGuard = require('./class/AuthGuard');
-const ApiResponse = require('./class/ApiResponse');
 
 pollsApi
   .route('/polls')
-  .get((req, res, next) => {
-    Poll.all((err, results) => {
-      if(err) {
-        return next(err);
-      }
-      return res.json(new ApiResponse({
-        req,
-        success: true,
-        data: results
-      }));
-    });
-  })
+  .get(PollController.all)
   .post(
     AuthGuard.tokenRequired, 
     ValidatorGuard.sanitizeBody,
@@ -30,39 +18,36 @@ pollsApi
       ValidatorGuard.fieldRequired('options')
     ],
     ValidatorGuard.collectErrors,
-    (req, res, next) => {
-      // After sanityzing the request body, the options array posted by the user is converted to a string,
-      // Let's convert it back to an array
-      const options = req.body.options.split(',');
-      Poll.create(req.user.id, req.body.title, options, (err, results) => {
-        if(err) {
-          console.log(err);
-          return next(err);
-        }
-        return res.json(new ApiResponse({
-          req,
-          success: true,
-          data: results
-        }));
-      });
-    });
+    PollController.add
+  );
 
 pollsApi
   .route('/polls/:id')
-  .get((req, res, next) => {
-    res.json({desc: 'get a poll by id', scope: 'public'});
-  })
-  .patch((req, res, next) => {
-    res.json({desc: 'update a poll', scope: 'auth users and admins'});
-  })
-  .delete((req, res, next) => {
-    res.json({desc: 'delete a poll by id', scope: 'auth users and admins'});
-  })
+  .all(ValidatorGuard.sanitizeIds)
+  .get(PollController.get('id'))
+  .patch(
+    AuthGuard.tokenRequired,
+    AuthGuard.authorIsUser,
+    ValidatorGuard.sanitizeBody,
+    ValidatorGuard.filterBody(['title', 'options']),
+    [
+      ValidatorGuard.fieldRequired('title'),
+      ValidatorGuard.fieldRequired('options')
+    ],
+    ValidatorGuard.collectErrors,
+    PollController.update
+  )
+  .delete(
+    AuthGuard.tokenRequired,
+    AuthGuard.authorIsUser,
+    PollController.remove
+  )
 
 pollsApi
   .route('/polls/users/:id')
-  .get((req, res, next) => {
-    res.json({desc: 'get a user polls', scope: 'public'});
-  })
+  .get(
+    ValidatorGuard.sanitizeIds,
+    PollController.get('id', 'Users')
+  )
 
 module.exports = pollsApi;
